9d941df0239572789982c78d2a2f80edc3ad3e31
Lizhi Ma <30405935@qq.com>
2018-09-02 20:36:49 +0800

    update

--- tool.py
+++ tool.py
@@ -130,7 +130,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("/Users/ddc/Desktop/MyBlog/themes/next/source/lib/album/data.json","w") as fp:
+    with open("E://YourBlog/themes/next/source/lib/album/data.json","w") as fp:
         json.dump(final_dict, fp)
 def cut_photo():
     """裁剪算法
